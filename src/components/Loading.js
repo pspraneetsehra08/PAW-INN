cd4dfb88effb0d0b0c2d6114388d6c5a40980f0c
Praneet Singh Sehra <68951839+pspraneetsehra08@users.noreply.github.com>
2023-05-27 00:14:52 +0530

    Add files via upload

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import loadingGif from "../images/gif/loading-gear.gif"
+
+export default function Loading() {
+    return (
+        <div className="loading">
+            <h4>Rooms data Loading...</h4>
+            <img src={loadingGif} alt="" />
+        </div>
+    )
+}
